933d026e0c503205ef5b908a269c72282ed8640d
Инна <anni31717213@gmail.com>
2022-01-15 23:58:59 +0200

    изменила 8.1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -10,34 +10,20 @@ let galleryEl = document.querySelector('.gallery');
 
 const imgEl = galleryItems
   .map(
-    img => `<div class="gallery__item">
-                <a class="gallery__link" href=${img.original}>
-                    <img
-                    class="gallery__image"
-                    src=${img.preview}
-                    data-source=${img.original}
-                    alt=${img.description}
-                    />
-                </a>
-            </div>`,
+    img =>  `<a class="gallery__link" href=${img.original}>
+                <img class="gallery__image" src=${img.preview} 
+                data-source=${img.original} alt=${img.description}/>
+            </a>`,
   )
   .join('');
 
 galleryEl.insertAdjacentHTML('beforeend', imgEl);
 
 for (let i = 0; i < document.getElementsByClassName('gallery__item').length; i++) {
-  document.getElementsByClassName('gallery__link')[i].addEventListener('click', function (e) {
+  document.getElementsByClassName('gallery__item')[i].addEventListener('click', function (e) {
     e.preventDefault();
     return false;
   });
-  const currentImg = document.getElementsByClassName('gallery__image')[i].dataset.source;
-  document.getElementsByClassName('gallery__item')[i].onclick = () => {
-    const imageLibrary = basicLightbox.create(`<img width="1280" height="720" src=${currentImg}>`);
-    imageLibrary.show();
-    document.addEventListener('keyup', e => {
-      if (e.code === 'Escape') imageLibrary.close();
-    });
-  };
 }
 
 const lightbox = new SimpleLightbox('.gallery a', {
