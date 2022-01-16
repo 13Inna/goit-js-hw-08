61f8296a66ed70c14db18521beec855d1c3a35eb
Инна <anni31717213@gmail.com>
2022-01-16 16:03:42 +0200

    вернула первоначальную версию 8\3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,32 +1,38 @@
-import throttle from 'lodash.throttle';
-const formData = {};
-let refs = { 
-    form: document.querySelector('.feedback-form'),
-    email: document.querySelector('.feedback-form input'),
-    email: document.querySelector('.feedback-form textarea'),
-}
-refs.form.addEventListener('submit', formSubmit);
-refs.form.addEventListener('input', throttle(formInput, 500));
-
-populateOnFormInput();
-
-function formSubmit(e) {
-    e.preventDefault();
-    e.currentTarget.reset();
-    localStorage.removeItem('feedback-form-state');
-    console.log("Delete text");
-}
-function formInput(e) {
-    formData[e.target.name] = e.target.value;
-    console.log(formData);
-    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
-}
-function populateOnFormInput() {
-  const userData = JSON.parse(localStorage.getItem(STORAGE_KEY));
-   if(userData && Object.values(userData) !== [ ] ) {
-    form.email.value = userData.email;
-    form.message.value = userData.message;
-    formData.email = userData.email;
-    formData.message = userData.message;
-   }
-}
+import throttle from 'lodash/throttle';
+	
+	const _ = require('lodash');
+	const formInput = document.querySelector('.feedback-form');
+	
+	let userInput = { email: '', password: '' };
+	// Took values from local storage if they present and put into userInput
+	if (localStorage.getItem('feedback-form-state')) {
+	  userInput = JSON.parse(localStorage.getItem('feedback-form-state'));
+	}
+	// if userInput is not empty fill fields in form
+	if (userInput.email.length > 0 || userInput.password.length > 0) {
+	  formInput.elements.namedItem('email').value = userInput.email;
+	  formInput.elements.namedItem('message').value = userInput.password;
+	}
+	
+	// Store user input
+	
+	function updateValue(e) {
+	  userInput.email = formInput.elements.namedItem('email').value;
+	  userInput.password = formInput.elements.namedItem('message').value;
+	  localStorage.setItem('feedback-form-state', JSON.stringify(userInput));
+	}
+	
+	var throttled = _.throttle(updateValue, 500);
+	
+	formInput.addEventListener('input', throttled);
+	
+	// Clear storage and fields in SUBMIT button pressed
+	function refreshFields(e) {
+	  e.preventDefault();
+	  console.log(`email: ${userInput.email}, password: ${userInput.password}`);
+	  formInput.elements.namedItem('email').value = '';
+	  formInput.elements.namedItem('message').value = '';
+	  localStorage.clear();
+	}
+	
+	formInput.addEventListener('submit', refreshFields);
